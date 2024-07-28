c8273cbb6773909cbeb883596c19ef324f01d95a
Svraj <74090764+Svrajj@users.noreply.github.com>
2024-07-28 18:42:12 +0530

    Add files via upload

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+    providers: [
+      {
+        domain: "https://upward-bonefish-9.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+  
+  export default authConfig;
---
+++ next.config.mjs
@@ -0,0 +1,25 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    typescript: {
+      ignoreBuildErrors: true
+    },
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'lovely-flamingo-139.convex.cloud'
+        },
+        {
+          protocol: 'https',
+          hostname: 'hidden-emu-969.convex.cloud'
+  
+        },
+        {
+          protocol: 'https',
+          hostname: 'img.clerk.com'
+        },
+      ]
+    }
+  };
+  
+  export default nextConfig;
